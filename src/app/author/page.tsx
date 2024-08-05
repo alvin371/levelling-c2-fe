21c84da1f10951edc9ea0e54a3edb92830c6ae9e
Nemixe <rafel.ami@gmail.com>
2024-08-06 00:09:44 +0700

    fix: remove dummy page

--- src/app/author/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthorClient from "./_components/client";
-
-const AuthorPage = async () => {
-  const author = await fetch("http://localhost:3000/api/authors");
-  const authorJson = await author.json();
-  return <AuthorClient author={authorJson.data.authors} />;
-};
-
-export default AuthorPage;
