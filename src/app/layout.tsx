0d75aca48df3e865737409c7c622923e73eb00b2
dot <macairm1@dot.co.id>
2024-09-09 14:02:28 +0700

    fix: issue error building

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Layout from "@/components/layout";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { ReactQueryProvider } from "@/components/providers/react-query";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <ReactQueryProvider>
           <AntdRegistry>
-            <Layout>{children}</Layout>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Layout>{children}</Layout>
+            </Suspense>
           </AntdRegistry>
         </ReactQueryProvider>
       </body>
